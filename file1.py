caba6c19635fe0b681ef654bf5a69a18ea36ac54
sushmas1309 <116005862+sushmas1309@users.noreply.github.com>
2022-10-17 20:04:57 +0530

    a

--- file1.py
+++ file1.py
@@ -4,6 +4,30 @@ Spyder Editor
 
 This is a temporary script file.
 """
+# -*- coding: utf-8 -*-
+#data structures
+#are structures which can hold some data together. used to store collection of related data
+#types - list, tuple, dictionary, set
 
-a=5
-print(a+3)
+#%%
+#list
+#ordered collection of items; sequence of items in a list
+shoplist =['apple','carrot','mango', 'banana']
+shoplist
+len(shoplist)
+
+#run next 2 lines together
+for item in shoplist:
+    print(item, end =' ')
+
+#add item to list
+shoplist.append('rice')
+shoplist
+
+#sort
+shoplist.sort()  #inplace sort
+shoplist
+
+#index/select
+shoplist[0]
+shoplist[0:4]
